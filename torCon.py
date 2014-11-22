e254666e3a4351382ef726f899e35821ba207be4
Kumar Vikramjeet <vik001ind@gmail.com>
2014-11-22 01:31:02 -0500

    v2

--- torCon.py
+++ torCon.py
@@ -2,20 +2,22 @@ import socks
 import socket
 import requests
 
+from stem import Signal
+from stem.control import Controller
+	
 def create_connection(address, timeout=None, source_address=None):
-    sock = socks.socksocket()
-    sock.connect(address)
-    return sock
+    	with Controller.from_port(port = 9151) as controller:
+  		controller.authenticate()
+		controller.signal(Signal.NEWNYM)
+    	sock = socks.socksocket()
+    	sock.connect(address)
+    	return sock
 
-socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 9050)
+socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 9150)
 
 # patch the socket module
 socket.socket = socks.socksocket
 socket.create_connection = create_connection
-
-# import urllib2
-
-# print urllib2.urlopen('http://checkip.dyndns.com/').read()
 
 response = requests.get('http://checkip.dyndns.com/')
 print response.elapsed
